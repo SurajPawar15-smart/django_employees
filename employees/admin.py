f9530136e2a381778268f07bc85a5916c9474522
SurajPawar15-smart <supersuraj143@gmail.com>
2025-11-24 10:35:21 +0530

    admin employees added

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Department, Employee
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'email', 'designation', 'salary', 'joining_date', 'manager', 'is_active']
+    list_filter = ['manager']
+    search_fields = ['full_name', 'email']
+    list_editable = ['is_active', 'salary']
+    list_display_links = ['full_name', 'email']
+    ordering = ["-id"]
+
+admin.site.register(Department)
+admin.site.register(Employee, EmployeeAdmin)
